9d74d378621ead87fd5e866e0376d23824f1d399
J Fav <jfavorito@outlook.com>
2020-07-16 21:19:59 -0700

    delete space

--- employee-table/src/components/table/Table.js
+++ employee-table/src/components/table/Table.js
@@ -7,7 +7,7 @@ function Table(props) {
         <tbody>
             {props.friends.map(friend => (
                 <tr className="row" key={friend.id}>
-                    <td className=" col-lg-3"><img className="table-img" alt={friend.name} src={friend.image} /></td>
+                    <td className="col-lg-3"><img className="table-img" alt={friend.name} src={friend.image} /></td>
                     <td className="col-lg-3">{friend.firstName}</td>
                     <td className="col-lg-3">{friend.lastName}</td>
                     <td className="col-lg-3">{friend.phoneNumber}</td>
