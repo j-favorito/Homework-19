f7103d625924c77c03c1edea4bc375fd098e3c85
J Fav <jfavorito@outlook.com>
2020-07-16 21:21:34 -0700

    made more responsive

--- employee-table/src/components/header/Header.js
+++ employee-table/src/components/header/Header.js
@@ -6,10 +6,10 @@ function Header(props) {
         <>
             <thead>
                 <tr className="row">
-                    <th className="col-lg-3">Sort By:</th>
-                    <th onClick={props.firstHandleClick} className="table-head col-lg-3">First Name</th>
-                    <th onClick={props.lastHandleClick} className="table-head col-lg-3">Last Name</th>
-                    <th onClick={props.phoneHandleClick} className="table-head col-lg-3">Phone Number</th>
+                    <th className="col-sm-3">Sort By:</th>
+                    <th onClick={props.firstHandleClick} className="table-head col-sm-3">First Name</th>
+                    <th onClick={props.lastHandleClick} className="table-head col-sm-3">Last Name</th>
+                    <th onClick={props.phoneHandleClick} className="table-head col-sm-3">Phone Number</th>
                 </tr>
             </thead>
         </>
--- employee-table/src/components/table/Table.js
+++ employee-table/src/components/table/Table.js
@@ -7,10 +7,10 @@ function Table(props) {
         <tbody>
             {props.friends.map(friend => (
                 <tr className="row" key={friend.id}>
-                    <td className="col-lg-3"><img className="table-img" alt={friend.name} src={friend.image} /></td>
-                    <td className="col-lg-3">{friend.firstName}</td>
-                    <td className="col-lg-3">{friend.lastName}</td>
-                    <td className="col-lg-3">{friend.phoneNumber}</td>
+                    <td className="col-sm-3"><img className="table-img" alt={friend.name} src={friend.image} /></td>
+                    <td className="col-sm-3">{friend.firstName}</td>
+                    <td className="col-sm-3">{friend.lastName}</td>
+                    <td className="col-sm-3">{friend.phoneNumber}</td>
                 </tr>
             ))}
             </tbody>
